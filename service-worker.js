9f06b0f5525c510e849d47b51b8281cf315426db
shunsukemitani <shun.mitani.smile04@gmail.com>
2025-09-25 23:38:51 +0900

    Update build script for universal mac app

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'time-q-cache-v2'; // バージョンを上げておく
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/script.js',
+    '/signature_pad.js',
+    '/icon-192x192.png',
+    '/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+            )
+    );
+});
+
+// 古いキャッシュを削除する処理を追加
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
